af8bd08b784da9ac311fe61455e44c348bdaffe3
NickPan7ov <ni3k.pan7ov@mail.ru>
2025-02-07 11:04:40 +0300

    Add class DB

---
+++ PR05_GIT_Panfilov/Db.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR05_GIT_Panfilov
+{
+    class Db
+    {
+        static void bd()
+        {
+            double ok;
+        }
+    }
+}
